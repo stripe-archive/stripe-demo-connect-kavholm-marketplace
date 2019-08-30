a057cc76834637e2b47faa22d5474cd291467c82
Kenneth Auchenberg <auchenberg@stripe.com>
2019-08-29 18:57:35 -0700

    Fix hard coded auth redirect_uri

--- pages/api/signup/stripe.js
+++ pages/api/signup/stripe.js
@@ -1,12 +1,13 @@
 const redirect = require('micro-redirect');
 
 import config from '../../../helpers/stripe';
+import getHost from '../../../utils/get-host';
 
 export default (req, res) => {
   let clientId = config.stripe.clientId;
   let state = '';
 
-  const redirect_uri = 'http://localhost:3000/stripe/callback';
+  const redirect_uri = getHost(req) + '/stripe/callback';
   const location = `https://connect.stripe.com/express/oauth/authorize?response_type=code&redirect_uri=${redirect_uri}&client_id=${clientId}&state=${state}&scope=read_write`;
 
   redirect(res, 302, location);

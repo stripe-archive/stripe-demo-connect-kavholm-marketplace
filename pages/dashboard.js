c06c775d01c417094e15821de9ce750907136be9
Kenneth Auchenberg <auchenberg@stripe.com>
2019-08-30 18:48:14 -0700

    New design on /dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -74,19 +74,61 @@ class Dashboard extends React.Component {
     return (
       <Layout isAuthenticated={this.props.isAuthenticated}>
         <div className="dashboard ">
-          <h2>Dashboard</h2>
+          <div className="row">
+            <div className="col-12">
+              <div className="media user-details">
+                <img src="/static/avatar.png" height="66" className="mr-3" />
+                <div className="media-body">
+                  <div className="user-details-body align-middle">
+                    <h5 className="mt-0">{this.props.profile.email}</h5>
+                    <p className="text-secondary">
+                      {this.props.profile.country}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <hr className="bg-light" />
 
           <div className="row">
             <div className="col-8">
-              <p>Welcome, {this.props.profile.email}</p>
+              <div className="row">
+                <div className="col-8">
+                  <div className="clearfix">
+                    <h4>Your listings</h4>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <a
+                    href="/listings/new"
+                    className="btn btn-primary btn-new-listing"
+                  >
+                    New
+                  </a>
+                </div>
+              </div>
+
+              <ul className="listings-list">
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
             </div>
 
-            <div className="col-4 text-right">
-              <a href="" className="btn btn-primary">
-                See payouts on Stripe
-              </a>
+            <div className="col-4">
+              <h4>Booking requests</h4>
+
+              <ul className="booking-list">
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
             </div>
           </div>
+
+          <hr className="bg-light" />
 
           <h5>Profile details:</h5>
           <pre className="profile-details bg-light">
@@ -103,6 +145,69 @@ class Dashboard extends React.Component {
             padding: 20px;
             overflow: auto;
             max-height: 700px;
+          }
+
+          .user-details {
+            font-size; 14px;
+          }
+
+          .user-details-body {
+            padding-top: 10px;
+          }
+
+          .user-details h5 {
+            font-size: 16px;
+            margin: 0;
+          }
+
+          .user-details p {
+            font-size: 12px;
+          }
+
+          .dashboard hr {
+            margin-bottom: 50px;
+          }
+
+          .dashboard h4 {
+            font-size: 18px;
+            margin-bottom: 30px;
+          }
+
+          .booking-list {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+          }
+
+          .booking-list li {
+            height: 30px;
+
+            border: 0;
+            margin-bottom: 30px;
+            background: #f6f6f6;
+          }
+
+          .listings-list {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-gap: 30px;
+            grid-auto-rows: minmax(100px, auto);
+          }
+
+          .listings-list li {
+            height: 325px;
+
+            border: 0;
+            background: #f6f6f6;
+          }
+
+          .btn-new-listing {
+            float: right;
+            margin-top: -8px;
           }
         `}</style>
       </Layout>

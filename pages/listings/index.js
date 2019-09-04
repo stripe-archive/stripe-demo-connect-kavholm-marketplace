959cb3e484a4ffb1d3f6e398144f6f158367a063
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-04 15:43:52 -0700

    Add fake listings

--- pages/listings/index.js
+++ pages/listings/index.js
@@ -22,6 +22,9 @@ function ListingsList(props) {
 
         .listing-item img {
           width: 100%;
+          height: 100%;
+          object-fit: cover;
+          object-position: bottom;
         }
       `}</style>
     </li>

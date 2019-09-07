0a1c411a4f165d7f4ac29d98edb0c21f9cba8b9a
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-06 22:25:02 -0700

    Move /profile endpoint

---
+++ client/pages/api/profile/index.js
@@ -0,0 +1,17 @@
+import storage from '../../../helpers/storage';
+import requireAuthEndpoint from '../../../utils/requireAuthEndpoint';
+
+export default requireAuthEndpoint(async (req, res) => {
+  let authenticatedUserId = req.authToken.userId;
+
+  try {
+    let userAccount = storage
+      .get('users')
+      .find({userId: authenticatedUserId})
+      .pick('userId', 'avatar', 'fullName', 'email', 'stripe')
+      .value();
+    return res.status(200).json(userAccount);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});

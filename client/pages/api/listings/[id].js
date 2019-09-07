c04019ed5fc283162b1c431ee6b6737b51d453bd
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-06 21:53:54 -0700

    Listings doesnt require auth

--- client/pages/api/listings/[id].js
+++ client/pages/api/listings/[id].js
@@ -1,29 +1,16 @@
 import storage from '../../../helpers/storage';
-import {validateToken} from '../../../utils/authToken';
 
 export default async (req, res) => {
-  if (!('authorization' in req.headers)) {
-    return res.status(401).send('Authorization header missing');
-  }
-  const auth = await req.headers.authorization;
-  const {token} = JSON.parse(auth);
-  const decodedToken = validateToken(token);
+  let id = req.query.id;
 
-  if (decodedToken) {
-    let authenticatedUserId = decodedToken.userId;
-    let id = req.query.id;
+  try {
+    let listing = storage
+      .get('listings')
+      .find({id: id})
+      .value();
 
-    try {
-      let user = storage
-        .get('listings')
-        .find({id: id})
-        .value();
-
-      return res.status(200).json(user);
-    } catch (err) {
-      return res.status(400).json(err);
-    }
-  } else {
-    return res.status(400).json({message: 'invalid token'});
+    return res.status(200).json(listing);
+  } catch (err) {
+    return res.status(400).json(err);
   }
 };

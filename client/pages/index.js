c73582d1a42f276f74a2b58d70ef0d3296122a9f
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-06 19:56:11 -0700

    Fix refs

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from 'next/link';
-import Modal from '../components/modal';
 import Layout from '../components/layout';
 
 class Home extends React.Component {

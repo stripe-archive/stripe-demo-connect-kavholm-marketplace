a609f3a38fc438aefc6f2117d4c7891d1e519d84
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-07 17:41:38 -0700

    Extract dashboardHeader

---
+++ client/components/dashboardHeader.js
@@ -0,0 +1,120 @@
+import React, {Component} from 'react';
+import BookingModal from './bookingModal';
+import {Elements, StripeProvider, injectStripe} from 'react-stripe-elements';
+import getConfig from 'next/config';
+
+import API from '../helpers/api';
+
+class DashboardHeader extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stripe: null,
+    };
+  }
+
+  async handleDashboardLink() {
+    let req = await API.makeRequest('get', '/api/payouts/link');
+    window.open(req.url);
+  }
+
+  render() {
+    let {profile} = this.props;
+    let avatarUrl = profile ? profile.avatar : '/static/avatar.png';
+
+    let formattedBalance = '';
+
+    if (this.props.balance && this.props.balance) {
+      const locale = new Intl.NumberFormat().resolvedOptions().locale;
+      const formatter = new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: this.props.balance.currency,
+      });
+
+      formattedBalance = formatter.format(this.props.balance.amount / 100);
+    }
+
+    return (
+      <div className="dashboard-header">
+        <div className="row">
+          <div className="col-8">
+            <div className="media user-details">
+              <img src={avatarUrl} className="mr-3 avatar" />
+              <div className="media-body">
+                <div className="user-details-body align-middle">
+                  <h5 className="mt-0">{profile && profile.fullName}</h5>
+                  <p className="text-secondary">{profile && profile.email}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="align-middle stripe-dashboard">
+              <p className="label text-secondary">Balance</p>
+              <p className="balance">
+                {formattedBalance}{' '}
+                <a href="#" onClick={this.handleDashboardLink}>
+                  View in Stripe Dashboard ↗
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <style jsx>{`
+          .dashboard-header {
+            margin-bottom: 50px;
+            padding-bottom: 10px;
+            border-bottom: 1px solid rgb(229, 229, 229);
+          }
+
+          .user-details .avatar {
+            height: 55px;
+            border-radius: 55px;
+            align-self: center;
+          }
+
+          .user-details {
+            font-size: 14px;
+          }
+
+          .user-details-body {
+            padding-top: 10px;
+          }
+
+          .user-details h5 {
+            font-size: 16px;
+            margin: 0;
+          }
+
+          .user-details p {
+            font-size: 12px;
+          }
+
+          .stripe-dashboard {
+            font-size: 12px;
+            padding-top: 4px;
+          }
+
+          .stripe-dashboard .label {
+            font-size: 16px;
+            margin: 0;
+            padding: 0;
+          }
+
+          .stripe-dashboard .balance {
+            margin: 0;
+            padding: 0;
+            font-size: 16px;
+            line-height: 1;
+          }
+
+          .stripe-dashboard a {
+            font-size: 12px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
+export default DashboardHeader;
--- client/pages/profile/index.js
+++ client/pages/profile/index.js
@@ -3,6 +3,7 @@ import {redirect} from '../../utils/redirect';
 
 import Layout from '../../components/layout';
 import API from '../../helpers/api';
+import DashboardHeader from '../../components/dashboardHeader';
 
 class Profile extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class Profile extends React.Component {
   static async getInitialProps(context) {
     return {
       profile: await API.makeRequest('get', '/api/profile'),
+      balance: await API.makeRequest('get', '/api/profile/balance'),
     };
   }
 
@@ -33,52 +35,25 @@ class Profile extends React.Component {
         title="Profile"
       >
         <div className="profile">
-          <div className="row">
-            <div className="col-12">
-              <div className="media user-details">
-                <img src={avatarUrl} height="66" className="mr-3" />
-                <div className="media-body">
-                  <div className="user-details-body align-middle">
-                    <h5 className="mt-0">{profile && profile.fullName}</h5>
-                    <p className="text-secondary">{profile && profile.email}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <DashboardHeader
+            profile={this.props.profile}
+            balance={this.props.balance}
+          />
 
-          <hr className="bg-light" />
-
-          <h5>Profile details:</h5>
+          <h4>Your profile details</h4>
           <pre className="profile-details bg-light">
             <code>{JSON.stringify(profile, null, 2)}</code>
           </pre>
         </div>
         <style jsx>{`
-          .user-details {
-            font-size; 14px;
-          }
-
-          .user-details-body {
-            padding-top: 10px;
-          }
-
-          .user-details h5 {
-            font-size: 16px;
-            margin: 0;
-          }
-
-          .user-details p {
-            font-size: 12px;
-          }
-
-          .profile hr {
-            margin-bottom: 50px;
-          }
-
           .profile h4 {
-            font-size: 18px;
+            font-size: 24px;
+            font-weight: bold;
             margin-bottom: 30px;
+          }
+
+          .profile-details {
+            padding: 10px;
           }
         `}</style>
       </Layout>

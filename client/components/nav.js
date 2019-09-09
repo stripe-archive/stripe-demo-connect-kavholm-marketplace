cae37be14bb73ad48eb8c754f0c25653766023fd
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-08 17:24:29 -0700

    Add booking form to header

--- client/components/nav.js
+++ client/components/nav.js
@@ -18,12 +18,32 @@ class Nav extends React.Component {
       <div className={className}>
         <nav className="navbar navbar-fixed navbar-expand-lg navbar-light">
           <ul className="navbar-nav mr-auto">
-            <li className="navitem">
+            <li className="navitem d-flex">
               <Link href="/">
                 <a className="navbar-brand">
                   <img className="logo" src="/static/logo.svg" />
                 </a>
               </Link>
+            </li>
+
+            <li className="nav-item">
+              <div className="booking-form">
+                <input
+                  type="search"
+                  className="search"
+                  placeholder="Singapore"
+                ></input>
+                <input
+                  type="datepicker"
+                  className="date"
+                  placeholder="Oct 3-7"
+                ></input>
+                <select className="guests dropdown-toggle">
+                  <option>3 guests</option>
+                  <option>2 guests</option>
+                  <option>1 guests</option>
+                </select>
+              </div>
             </li>
           </ul>
 
@@ -38,7 +58,7 @@ class Nav extends React.Component {
             }
 
             .navbar {
-              margin: 30px 0;
+              margin: 30px 0 30px 0;
               padding: 0;
               height: 45px;
             }
@@ -50,6 +70,41 @@ class Nav extends React.Component {
 
             .logo {
               padding-right: 42px;
+              align-self: center;
+            }
+
+            .booking-form input {
+              border: 1px solid rgb(229, 229, 229);
+              border-radius: 6px;
+              margin-right: 15px;
+              padding: 0 10px 0 30px;
+              width: 150px;
+              font-size: 14px;
+            }
+
+            .booking-form select {
+              border: 1px solid rgb(229, 229, 229);
+              border-radius: 6px;
+              margin-right: 15px;
+              padding: 0 10px 0 30px;
+              font-size: 14px;
+              appearance: none;
+            }
+
+            .booking-form .search {
+              background: url(/static/search.svg) no-repeat scroll 7px 6px;
+              width: 200px;
+              background-size: 15px 15px;
+            }
+
+            .booking-form .date {
+              background: url(/static/cal.svg) no-repeat scroll 7px 6px;
+              background-size: 15px 15px;
+            }
+
+            .guests {
+              background: url(/static/people.svg) no-repeat scroll 7px 6px;
+              background-size: 15px 15px;
             }
           `}</style>
         </nav>

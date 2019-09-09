da852f4bf1a138626dab57859fcbdd6d46f1847b
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-09 14:59:30 -0700

    Update booking dates

--- client/components/bookingModal.js
+++ client/components/bookingModal.js
@@ -15,8 +15,8 @@ class BookingModal extends Component {
         currency: 'usd',
         amount: props.amount,
         listingId: 26,
-        startDate: '09/05/2019',
-        endDate: '09/05/2019',
+        startDate: '10/03/2019',
+        endDate: '10/07/2019',
       },
     };
   }

972695d23afee186a28559697746d6ef3bcad2f3
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-09 10:35:26 -0700

    Tweaks to home height

--- client/pages/index.js
+++ client/pages/index.js
@@ -26,12 +26,14 @@ class Home extends React.Component {
         <style jsx>{`
           .home {
             width: 100%;
+            min-height: 800px;
             position: absolute;
             top: 110px;
             left: 0;
             right: 0;
             bottom: 0;
           }
+
           .splash-image {
             width: 100%;
             height: 100%;

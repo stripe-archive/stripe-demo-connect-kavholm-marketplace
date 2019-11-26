ae2813a432d5142666854c38f5c0d8c002b04895
Kenneth Auchenberg <auchenberg@stripe.com>
2019-11-25 16:54:07 -0800

    Add /api/bookings

---
+++ client/pages/api/bookings/index.js
@@ -0,0 +1,17 @@
+import storage from '../../../helpers/storage';
+import requireAuthEndpoint from '../../../utils/requireAuthEndpoint';
+
+export default requireAuthEndpoint(async (req, res) => {
+  let authenticatedUserId = req.authToken.userId;
+
+  try {
+    let bookings = storage
+      .get('bookings')
+      .filter({userId: authenticatedUserId})
+      .value();
+
+    return res.status(200).json(bookings);
+  } catch (err) {
+    return res.status(400).json(err);
+  }
+});
